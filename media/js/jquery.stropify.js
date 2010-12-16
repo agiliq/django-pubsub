c682eda193733e9830d83ce0515e7345a673b531
Javed Khan <javed@agiliq.com>
2010-12-16 16:05:48 +0530

    no longer a jquery plugin,

--- media/js/jquery.stropify.js
+++ media/js/jquery.stropify.js
@@ -1,106 +1,94 @@
-//(function($) {
-
-    PubSubClient.prototype = {
-        
-        /* connect to the xmpp server
-         *
-         * params:
-         *   
-         *   username: username of xmpp account without domain
-         *   password: plaintext password of the account
-         *
-         * for anonymous logins
-         *
-         *   nick: the nickname to be displayed after anoymous login
-         *
-         * returns:
-         *   Strophe.Connection object
-         */
-        connect: function(username, password) {
-            this._conn = new Strophe.Connection(settings.BOSH_SERVICE);
-            this._jid = username + '@' + settings.DOMAIN + '/' + settings.RESOURCE;
-            if ( typeof(username) != 'undefined' &&  username.length) {
-                this._conn.connect(this._jid, password, this._on_connect(this));
-                this._nick = username;
-            }
-            else {
-                // anonymous login
-                this._conn.connect(settings.DOMAIN, null, this._on_connect(this));
-                this._nick = this.options.nick;
-            }
-            return this._conn;
-        },
-
-        /* disconnect from the xmpp server */
-        disconnect: function() {
-            this._conn.disconnect();
-            this._on_disconnect();
-        },
-
-        /* callback fired when connection status changes */
-        _on_connect: function(context) {
-            return function (status, condition) {
-                if (settings.DEBUG) {
-                    console.log("status is ", status);
-                }
-                 if (status == Strophe.Status.CONNECTED) {
-                    context.subscribe(context.options.node);
-                }
-            };
-        },
-
-        /*
-         *subscribes user to given node
-         */
-        subscribe: function(node) {
-            return this._conn.pubsub.subscribe(this._jid, settings.PUBSUB_SERVICE, 
-                    node, {}, this._handle_event, this._handle_event);
-        },
-
-
-        /*
-         *publishes items to node
-         */
-        publish: function(node, items) {
-            return this._conn.pubsub.publish(this._jid, settings.PUBSUB_SERVICE,
-                    node, items, this._handle_event);
-        },
-
-
-        /*
-         *handles pubsub event
-         */
-        _handle_event: function(stanza_xml) {
-            console.log(stanza_xml);
-            return true;
-        },
-
-        /* callback fired before disconnect */
-        _on_disconnect: function(status) {
-            //this.leave();
+PubSubClient.prototype = {
+    
+    /* connect to the xmpp server
+        *
+        * params:
+        *   
+        *   username: username of xmpp account without domain
+        *   password: plaintext password of the account
+        *
+        * for anonymous logins
+        *
+        *   nick: the nickname to be displayed after anoymous login
+        *
+        * returns:
+        *   Strophe.Connection object
+        */
+    connect: function(username, password) {
+        this._conn = new Strophe.Connection(settings.BOSH_SERVICE);
+        this._jid = username + '@' + settings.DOMAIN + '/' + settings.RESOURCE;
+        if ( typeof(username) != 'undefined' &&  username.length) {
+            this._conn.connect(this._jid, password, this._on_connect(this));
+            this._nick = username;
         }
+        else {
+            // anonymous login
+            this._conn.connect(settings.DOMAIN, null, this._on_connect(this));
+            this._nick = this.options.nick;
+        }
+        return this._conn;
+    },
 
-    };
+    /* disconnect from the xmpp server */
+    disconnect: function() {
+        this._conn.disconnect();
+        this._on_disconnect();
+    },
 
-    /* PubSubClient class that initializes 
-     * event handlers, auto connects to the server
-     * and auto joins 'room' of options
-     *
-     */
-    function PubSubClient(options) {
-        var that = this;
-        this.options = options;
-        var connection = that.connect(options.username, options.password);
+    /* callback fired when connection status changes */
+    _on_connect: function(context) {
+        return function (status, condition) {
+            if (settings.DEBUG) {
+                console.log("status is ", status);
+            }
+                if (status == Strophe.Status.CONNECTED) {
+                context.subscribe(context.options.node);
+            }
+        };
+    },
+
+    /*
+        *publishes items to node
+        */
+    publish: function(node, items) {
+        return this._conn.pubsub.publish(this._jid, settings.PUBSUB_SERVICE,
+                node, items, this._handle_event(this));
+    },
+
+
+    /*
+        *subscribes user to given node
+        */
+    subscribe: function(node) {
+        return this._conn.pubsub.subscribe(this._jid, settings.PUBSUB_SERVICE, 
+                node, {}, this._handle_event(this), this._handle_event(this));
+    },
+
+
+    /*
+        *handles pubsub event
+        */
+    _handle_event: function(context) {
+        return function(stanza_xml) {
+            console.log(stanza_xml);
+            context._last_xml = stanza_xml;
+            return true;
+        };
+    },
+
+    /* callback fired before disconnect */
+    _on_disconnect: function(status) {
+        //this.leave();
     }
 
-    //// TODO: return jquery element like a good plugin
+};
 
-    //$.fn.stropify = function(opts) {
-        //var defaults = {
-        //};
-        //var options = $.extend(defaults, opts);
-        //return new PubSubClient(options);
-    //};
-
-//})(jQuery);
+/* PubSubClient class that initializes 
+    * event handlers, connects to the server
+    */
+function PubSubClient(options) {
+    var that = this;
+    this.options = options;
+    var connection = that.connect(options.username, options.password);
+}
 
