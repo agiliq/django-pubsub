f0a217dd815936c47484dc529b8de65628dad28a
Javed Khan <javed@agiliq.com>
2010-12-22 15:41:09 +0530

    settings cleanup

--- media/js/pubsub.settings.js
+++ media/js/pubsub.settings.js
@@ -2,8 +2,7 @@ settings = {
 
     BOSH_SERVICE: '/xmpp-httpbind',
     DOMAIN: 'localhost',
-    RESOURCE: 'strophe_firefox',
+    RESOURCE: 'strophejs',
     PUBSUB_SERVICE: 'pubsub.localhost',
-    DEBUG: true,
-    MESSAGE_SOUND_URL: '/site_media/speeqewebclient/KDE-Im-Irc-Event.ogg'
+    DEBUG: true
 };
